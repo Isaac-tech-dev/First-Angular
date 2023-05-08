2df92361a61ca7c75c09c0e56b8a1eda1c74769c
Isaac Ayeni <ayeniisaac1on1@gmail.com>
2023-05-08 10:44:10 +0100

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MaterialModule } from './shared/material/material.module';
+import { ClientComponent } from './pages/client/client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
